1adfa11d01b25b41dd2e527f10e7514fe1c71c29
Harivansh <hello@harivan.sh>
2019-10-14 15:17:30 +0400

    ...

---
+++ scripts/modules/DirectoryStructure.py
@@ -0,0 +1,14 @@
+import os
+
+
+
+
+class DirectoryStructure:
+    def __init__(self, structure):
+        self.structure = structure
+
+    def create(self):
+        pass
+
+    def __checkStructure(self):
+        pass
