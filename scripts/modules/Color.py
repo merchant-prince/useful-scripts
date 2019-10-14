05875e63c1f0bf37851e7906cf569ac08118f68e
Harivansh <hello@harivan.sh>
2019-10-14 18:13:47 +0400

    refactor Color.py

--- scripts/modules/Color.py
+++ scripts/modules/Color.py
@@ -12,6 +12,12 @@ class Color:
             "RESET": 0
     }
 
+    def __create(self, colorCode):
+        CSI = "\033["
+        END = "m"
+
+        return f"{CSI}{colorCode}{END}"
+
     def __message(self, colorCode):
         FOREGROUND = "40;38;5"
 
@@ -22,18 +28,14 @@ class Color:
 
         return f"{BACKGROUND};{colorCode}"
 
-    def __create(self, colorCode):
-        CSI = "\033["
-        END = "m"
-
-        return f"{CSI}{colorCode}{END}"
-
     def __getattr__(self, name):
         tokens = name.split("_")
 
         if len(tokens) == 1:
             if tokens[0] == "RESET":
                 return self.__create(Color.CODE["RESET"])
+            else:
+                raise ValueError(f"{name} is not a valid attribute")
 
         elif len(tokens) == 2:
             colorType, displayType = tokens
@@ -42,24 +44,36 @@ class Color:
                 if colorType == "SUCCESS":
                     return self.__create(self.__banner(Color.CODE["CHARTREUSE"]))
 
-                if colorType == "INFO":
+                elif colorType == "INFO":
                     return self.__create(self.__banner(Color.CODE["CYAN"]))
 
-                if colorType == "WARNING":
+                elif colorType == "WARNING":
                     return self.__create(self.__banner(Color.CODE["YELLOW"]))
 
-                if colorType == "ERROR":
+                elif colorType == "ERROR":
                     return self.__create(self.__banner(Color.CODE["RED_SINDOOR"]))
+
+                else:
+                    raise ValueError(f"{name} is not a valid attribute")
 
             elif displayType == "MESSAGE":
                 if colorType == "SUCCESS":
                     return self.__create(self.__message(Color.CODE["CHARTREUSE"]))
 
-                if colorType == "INFO":
+                elif colorType == "INFO":
                     return self.__create(self.__message(Color.CODE["CYAN"]))
 
-                if colorType == "WARNING":
+                elif colorType == "WARNING":
                     return self.__create(self.__message(Color.CODE["YELLOW"]))
 
-                if colorType == "ERROR":
+                elif colorType == "ERROR":
                     return self.__create(self.__message(Color.CODE["RED_SINDOOR"]))
+
+                else:
+                    raise ValueError(f"{name} is not a valid attribute")
+
+            else:
+                raise ValueError(f"{name} is not a valid attribute")
+
+        else:
+            raise ValueError(f"{name} is not a valid attribute")
